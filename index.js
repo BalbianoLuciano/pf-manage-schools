e98876d0bed6bf0256ce9116f2ffacd1a4c800e7
unknown <ridaoc19@gmail.com>
2022-08-28 20:55:23 +0200

    ahi vamos

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function laCajaDePandora(num){
    }
    
    console.log(laCajaDePandora(128))
+
+
+   const Persona ={
+    nombre: "ricardo",
+    apellido: "david",
+    edad: 32,
+    grupo: "de otro"
+}
+
+const FuncionRequerida = ()=>{
+    return `${Persona.nombre} ${Persona.apellido}`
+}
+
+console.log(FuncionRequerida())
