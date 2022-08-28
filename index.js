25522146b36b5980938ee3ef6054950397ad7d1a
sergioglongo <sergiolongo@gmail.com>
2022-08-27 21:22:02 -0300

    funcion lacajadepandora

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    console.log("rama de Sergio");
-    }
+function laCajaDePandora(int){
+    if(int % 2 === 0)
+      return int.toString(2)
+     else 
+      return int.toString(16)
+   }
