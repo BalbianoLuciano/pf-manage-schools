640232cb7994509931eccd8d13cccb56092d234a
Balbianoluciano <balbiano06@gmail.com>
2022-08-28 15:44:09 -0300

    update box

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(num){
     // proximamente escribiremos codigo aqui
     //Cambio de Luciano
+    if(num % 2 === 0){
+        return num.toString(2)
+      } else {
+        return num.toString(16)
+      }
     }
