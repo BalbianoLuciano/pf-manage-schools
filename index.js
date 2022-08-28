18374792de54adf2c26cd638d14e76cc33f45514
Tucuman1515 <gerar.fornies@gmail.com>
2022-08-28 20:31:43 -0300

    nombre y apellido

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const funcionProbandoGit = (numero)=>{
-    if(numero % 2 === 0){
-      return numero.toString(2)
+const funcionProbandoGit = (numero) => {
+    if (numero % 2 === 0) {
+        return numero.toString(2)
     } else {
-      return numero.toString(16)
+        return numero.toString(16)
     }
-   }
+}
+
+const Persona = {
+    nombre: "Gerardo",
+    apellido: "Fornies",
+    edad: 32,
+    grupo: "27b"
+}
+
+const FuncionRequerida = () => {
+    return `${Persona.nombre} ${Persona.apellido}`
+}
+
+console.log(FuncionRequerida())
