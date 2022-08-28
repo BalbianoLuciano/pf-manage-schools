711791b2b9abaee92ff1c9ff7326cc5ac2ff1914
florlap <florencialapadatoff@gmail.com>
2022-08-27 21:50:21 -0300

    funcion calcular

---
+++ index.js
@@ -0,0 +1,11 @@
+function calculo(num){
+    if(num % 2 === 0){
+      return num.toString(2)
+    } else {
+      return num.toString(16)
+    }
+   }
+   
+   console.log(laCajaDePandora(128))
+
+
